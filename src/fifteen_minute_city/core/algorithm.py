2724cbdaf79a7a95945b900049320de56361b490
Pedro Jesus <pedrohenriquedejesus0419@gmail.com>
2026-06-25 17:16:01 -0300

    ruff format

--- src/fifteen_minute_city/core/algorithm.py
+++ src/fifteen_minute_city/core/algorithm.py
@@ -12,28 +12,28 @@ tags = [
     {"amenity": "bank"},
     {"amenity": "hospital"},
     {"amenity": "pharmacy"},
-    {"shop": "supermarket"}
+    {"shop": "supermarket"},
 ]
 
 hwy_speeds = {
-    'motorway': 3,
-    'trunk': 3,
-    'primary': 3,
-    'secondary': 3,
-    'tertiary': 3,
-    'residential': 3,
-    'service': 3
+    "motorway": 3,
+    "trunk": 3,
+    "primary": 3,
+    "secondary": 3,
+    "tertiary": 3,
+    "residential": 3,
+    "service": 3,
 }
 
 G = ox.add_edge_speeds(G, hwy_speeds=hwy_speeds)
 G = ox.add_edge_travel_times(G)
+
 
 def get_locations(tags: list) -> list:
 
     services_locations = []
 
     for tag in tags:
-
         places = ox.features_from_place(PLACE, tag)
 
         locations = {}
@@ -53,7 +53,9 @@ def get_locations(tags: list) -> list:
 
     return services_locations
 
+
 l = get_locations(tags)
+
 
 def get_times(services_location: list) -> dict:
 
@@ -61,10 +63,13 @@ def get_times(services_location: list) -> dict:
 
     for service in services_location:
         for name_service, location_service in service.items():
-            path_time, paths = nx.single_source_dijkstra(G, location_service, weight="travel_time")
+            path_time, paths = nx.single_source_dijkstra(
+                G, location_service, weight="travel_time"
+            )
             times[name_service] = (sum(path_time.values()) / len(path_time)) / 60
-    times['general_average'] = sum(times.values()) / len(times)
+    times["general_average"] = sum(times.values()) / len(times)
     return times
+
 
 l = get_locations(tags)
 t = get_times(l)
