6831cd44fd481027cd08085fec441e2be7112414
Pedro Jesus <pedrohenriquedejesus0419@gmail.com>
2026-06-28 15:12:54 -0300

    version 1.0.0 - output adjusted for better viewing and CLI added

--- src/fifteen_minute_city/core/algorithm.py
+++ src/fifteen_minute_city/core/algorithm.py
@@ -1,45 +1,20 @@
 import osmnx as ox
 import networkx as nx
+import typer
 
-PLACE = "Praia Grande, São Paulo, Brazil"
-
-G = ox.graph_from_place(PLACE, network_type="walk")
-
-tags = [
-    {"amenity": "bus_station"},
-    {"amenity": "school"},
-    {"amenity": "fuel"},
-    {"amenity": "bank"},
-    {"amenity": "hospital"},
-    {"amenity": "pharmacy"},
-    {"shop": "supermarket"},
-]
-
-hwy_speeds = {
-    "motorway": 3,
-    "trunk": 3,
-    "primary": 3,
-    "secondary": 3,
-    "tertiary": 3,
-    "residential": 3,
-    "service": 3,
-}
-
-G = ox.add_edge_speeds(G, hwy_speeds=hwy_speeds)
-G = ox.add_edge_travel_times(G)
+app = typer.Typer()
 
 
-def get_locations(tags: list) -> list:
+def get_locations(tags: list[dict], place: str) -> dict:
 
-    services_locations = []
+    locations = {}
 
     for tag in tags:
-        places = ox.features_from_place(PLACE, tag)
+        services_locations = []
+        places = ox.features_from_place(place, tag)
 
-        locations = {}
-
-        for idx, place in places.iterrows():
-            geom = place.geometry
+        for idx, local in places.iterrows():
+            geom = local.geometry
 
             if not hasattr(geom, "x"):
                 geom = geom.centroid
@@ -47,30 +22,89 @@ def get_locations(tags: list) -> list:
             lat = geom.y
             lon = geom.x
 
-            locations[place["name"]] = ox.distance.nearest_nodes(G, X=lon, Y=lat)
-
-        services_locations.append(locations)
-
-    return services_locations
-
-
-l = get_locations(tags)
-
-
-def get_times(services_location: list) -> dict:
-
-    times = {}
-
-    for service in services_location:
-        for name_service, location_service in service.items():
-            path_time, paths = nx.single_source_dijkstra(
-                G, location_service, weight="travel_time"
+            services_locations.append(
+                {local["name"]: ox.distance.nearest_nodes(G, X=lon, Y=lat)}
             )
-            times[name_service] = (sum(path_time.values()) / len(path_time)) / 60
-    times["general_average"] = sum(times.values()) / len(times)
-    return times
+
+        locations[tag["amenity"]] = services_locations
+
+    return locations
 
 
-l = get_locations(tags)
-t = get_times(l)
-print(t)
+def get_times(services_location: dict[dict]) -> dict:
+
+    result = {}
+
+    for type_service, services in services_location.items():
+        times = []
+        for service in services:
+            for name_service, location_service in service.items():
+                path_time, paths = nx.single_source_dijkstra(
+                    G, location_service, weight="travel_time"
+                )
+                times.append(
+                    {name_service: (sum(path_time.values()) / len(path_time)) / 60}
+                )
+        result[type_service] = times
+
+    return result
+
+
+def get_averages(times: dict) -> list:
+    general_average = []
+    average_total = 0
+    cont = 0
+    for type_service, services in times.items():
+        average_service = 0
+        for service in services:
+            ((name, avg),) = service.items()
+            average_service += avg
+            cont += 1
+        average_total += average_service
+        general_average.append({type_service: average_service / len(services)})
+    general_average.append({"general_average": average_total / cont})
+
+    return general_average
+
+
+@app.command()
+def run(place: str, net_type: str, hwy_speed: int):
+
+    typer.echo(
+        f"Local: {place}\nTipo de transporte: {net_type}\nVelocidade: {hwy_speed} km/h"
+    )
+    typer.echo("-" * 50)
+    global G
+    G = ox.graph_from_place(place, network_type=net_type)
+
+    tags = [
+        {"amenity": "bus_station"},
+        {"amenity": "school"},
+        {"amenity": "fuel"},
+        {"amenity": "bank"},
+        {"amenity": "hospital"},
+        {"amenity": "pharmacy"},
+        {"shop": "supermarket"},
+    ]
+
+    hwy_speeds = {
+        "motorway": hwy_speed,
+        "trunk": hwy_speed,
+        "primary": hwy_speed,
+        "secondary": hwy_speed,
+        "tertiary": hwy_speed,
+        "residential": hwy_speed,
+        "service": hwy_speed,
+    }
+
+    G = ox.add_edge_speeds(G, hwy_speeds=hwy_speeds)
+    G = ox.add_edge_travel_times(G)
+
+    results = get_times(get_locations(tags, place))
+
+    general_average = get_averages(results)
+    return typer.echo(general_average)
+
+
+if __name__ == "__main__":
+    app()
